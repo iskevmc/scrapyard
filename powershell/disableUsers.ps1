416f5ab0dcc26100cf0e1935dd5dc731889c526a
McLeod <iskevmc@gmail.com>
2021-05-22 04:18:08 -0400

    Dropped powershell stuff and renamed linux folder

--- powershell/disableUsers.ps1
+++
@@ -1,9 +0,0 @@
-##Description - Disable all users accounts in a specified OU
-
-Import-Module activedirectory
-
-Get-ADUser -Filter 'Name -like "*"' -SearchBase "OU=zz-Disabled Users,DC=company,DC=local" | Disable-ADAccount
-
-
-
-
